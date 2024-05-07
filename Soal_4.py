8f9a3231112d9d90294ce92910869fddbdd3847f
Ihsan Nafis HIdayat <145647779+IhsanNafis123@users.noreply.github.com>
2024-05-07 16:38:53 +0700

    Add files via upload

--- Soal_4.py
+++ Soal_4.py
@@ -1,35 +1,3 @@
-# def hitung_bmi(berat, tinggi):
-#     bmi = berat / (tinggi ** 2)
-#     return bmi
-
-# def menentukan_kategori(bmi):
-#    if 18.5 <= bmi < 25:
-#         return "Berat badan normal"
-#    elif 25 <= bmi < 30:
-#         return "Kelebihan berat badan"
-#    else:
-#         return "Obesitas"
-
-# def main():
-#     berat = float(input("Masukkan berat badan Anda (kg): "))
-#     tinggi = float(input("Masukkan tinggi badan Anda (cm): "))
-
-#     bmi = hitung_bmi(berat, tinggi)
-
-#     print(f"Berat Badan : {berat}")
-
-#     print(f"Tinggi Badan : {tinggi}")
-
-#     print("Nilai BMI Anda adalah:", bmi)
-
-#     if menentukan_kategori(bmi):
-#         print("Kategori BMI : Berat Badan Normal.")
-#     else:
-#         print("Kategori BMI : Berat Badan Tidak Normal.")
-
-# if __name__ == "__main__":
-#     main()
-
 def hitung_bmi(berat, tinggi):
     bmi = berat / ((tinggi / 100) ** 2)
     return bmi
